b9de59cccbc1a54f5f1853f7d0b5b872e1cea737
Lou <lou@Lous-iMac.home>
2018-10-30 19:45:54 -0400

    first

---
+++ main.rs
@@ -0,0 +1,47 @@
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: HashMap<u32, u32>,
+}
+impl <T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        *self.value.entry(arg).or_insert((self.calculation)(arg))
+        /*
+        match self.value.get(&arg).cloned() {
+            Some(v) => {
+                v
+            },
+            None => {
+                let v = (self.calculation)(arg);
+                self.value.insert(arg, v);
+                v
+            },
+        }
+        */
+    }
+}
+
+
+fn main() {
+    let mut test = Cacher::new(|arg| arg+1);
+
+    println!("{}", test.value(2));
+    println!("{}", test.value(2));
+    println!("{}", test.value(22));
+    println!("{}", test.value(22));
+}
+
